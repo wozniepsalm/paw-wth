250f4ed2947619fce365e06f75b5a97ebb5dfd76
wozniepsalm <majawozniak71@gmail.com>
2025-10-24 15:31:41 +0200

    lab_04 do zadania 3

--- project/myapp/admin.py
+++ project/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Book, Genre, Osoba, stanowisko
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Osoba)
+admin.site.register(stanowisko)
