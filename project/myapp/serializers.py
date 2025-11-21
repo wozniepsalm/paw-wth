4dfe6ed89320314c88cc8bf233d2f4d13a2dba65
wozniepsalm <majawozniak71@gmail.com>
2025-11-21 15:38:55 +0100

    zadania

---
+++ project/myapp/serializers.py
@@ -0,0 +1,144 @@
+from rest_framework import serializers
+from .models import Book, Author, Genre, MONTHS, BOOK_FORMATS
+from rest_framework.validators import UniqueTogetherValidator
+
+class BookSerializer(serializers.Serializer):
+    """Serializer dla modelu Book."""
+
+    # pole tylko do odczytu — ID książki
+    id = serializers.IntegerField(read_only=True)
+
+    # pole wymagane — tytuł książki
+    title = serializers.CharField(required=True)
+
+    # pole mapowane z klasy modelu, z podaniem wartości domyślnych
+    # zwróć uwagę na zapisywaną wartość do bazy dla default={wybór}[0] oraz default={wybór}[0][0]
+    # w pliku models.py BOOK_FROMATS oraz MONTHS zostały wyniesione jako stałe do poziomu zmiennych skryptu
+    # (nie wewnątrz modelu)
+
+    # pole z wyborem miesiąca publikacji
+    # MONTHS.choices pochodzi z modelu (Enum lub lista krotek)
+    publication_month = serializers.ChoiceField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
+
+    # pole z wyborem formatu książki (np. Paperback, Hardcover, eBook)
+    # BOOK_FORMATS zostało zdefiniowane jako stała w models.py
+    book_format = serializers.ChoiceField(choices=BOOK_FORMATS, default=BOOK_FORMATS[0][0])
+
+    # odzwierciedlenie pola w postaci klucza obcego
+    # przy dodawaniu nowego obiektu możemy odwołać się do istniejącego poprzez inicjalizację nowego obiektu
+    # np. author=Author({id}) lub wcześniejszym stworzeniu nowej instancji tej klasy
+    # klucz obcy — autor książki (może być null)
+    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), allow_null=True)
+
+    # klucz obcy — gatunek książki (może być null)
+    genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), allow_null=True)
+
+    # liczba dostępnych kopii książki
+    available_copies = serializers.IntegerField(default=1)
+
+    # przesłonięcie metody create() z klasy serializers.Serializer
+    # metoda create() — tworzenie nowego obiektu Book
+    def create(self, validated_data):
+        return Book.objects.create(**validated_data)
+
+    # przesłonięcie metody update() z klasy serializers.Serializer
+    # metoda update() — aktualizacja istniejącego obiektu Book
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.publication_month = validated_data.get('publication_month', instance.publication_month)
+        instance.book_format = validated_data.get('book_format', instance.book_format)
+        instance.author = validated_data.get('author', instance.author)
+        instance.genre = validated_data.get('genre', instance.genre)
+        instance.available_copies = validated_data.get('available_copies', instance.available_copies)
+        instance.save()
+        return instance 
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        # musimy wskazać klasę modelu
+        model = Book
+        # definiując poniższe pole możemy określić listę właściwości modelu,
+        # które chcemy serializować
+        fields = ['id', 'title', 'publication_month', 'book_format', 'author', 'genre', 'available_copies']
+        # definicja pola modelu tylko do odczytu
+        read_only_fields = ['id']
+
+    def validate_title(self, value):
+        if not value.istitle():
+            raise serializers.ValidationError(
+                "Tytuł książki powinien rozpoczynać się wielką literą!"
+            )
+        return value 
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+    def validate(self, data):
+        """
+        Walidacja całego obiektu autora.
+        Sprawdza poprawność formatu imienia, nazwiska i kodu kraju.
+        """
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        country = data.get('country')
+
+        # Imię i nazwisko powinny zaczynać się wielką literą
+        if first_name and not first_name.istitle():
+            raise serializers.ValidationError(
+                {"first_name": "Imię powinno rozpoczynać się wielką literą!"}
+            )
+
+        if last_name and not last_name.istitle():
+            raise serializers.ValidationError(
+                {"last_name": "Nazwisko powinno rozpoczynać się wielką literą!"}
+            )
+
+        # Kod kraju: dokładnie 2 wielkie litery
+        if country and (len(country) != 2 or not country.isupper()):
+            raise serializers.ValidationError(
+                {"country": "Kod kraju musi składać się z 2 wielkich liter, np. 'PL'."}
+            )
+
+        return data 
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Author.objects.all(),
+                fields=['first_name', 'last_name']
+            )
+        ] 
+    def validate(self, data):
+        """
+         Walidacja całego obiektu autora.
+        Sprawdza poprawność formatu imienia, nazwiska i kodu kraju.
+        """
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        country = data.get('country')
+
+        # Imię i nazwisko powinny zaczynać się wielką literą
+        if first_name and not first_name.istitle():
+            raise serializers.ValidationError(
+                {"first_name": "Imię powinno rozpoczynać się wielką literą!"}
+            )
+
+        if last_name and not last_name.istitle():
+            raise serializers.ValidationError(
+                {"last_name": "Nazwisko powinno rozpoczynać się wielką literą!"}
+            )
+
+        # Kod kraju: dokładnie 2 wielkie litery
+        if country and (len(country) != 2 or not country.isupper()):
+            raise serializers.ValidationError(
+                {"country": "Kod kraju musi składać się z 2 wielkich liter, np. 'PL'."}
+            )
+
+        return data
+
--- project/myapp/views.py
+++ project/myapp/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
+
+# określamy dostępne metody żądania dla tego endpointu
+@api_view(['GET', "POST"])
+def book_list(request):
+    """
+    Lista wszystkich obiektów modelu Book.
+    """
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def book_detail(request, pk):
+
+    """
+    :param request: obiekt DRF Request
+    :param pk: id obiektu Book
+    :return: Response (with status and/or object/s data)
+    """
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    """
+    Zwraca pojedynczy obiekt typu Book.
+    """
+    if request.method == 'GET':
+        serializer = BookSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # Create your views here.
